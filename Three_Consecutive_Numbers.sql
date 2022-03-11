9511dbcab56999223f8785624d90ea61af07938e
Pooja Trivedi <94572434+poojart58@users.noreply.github.com>
2022-03-10 21:16:05 -0800

    Update Three_Consecutive_Numbers.sql

--- Three_Consecutive_Numbers.sql
+++ Three_Consecutive_Numbers.sql
@@ -20,4 +20,24 @@
 -- | 1               |
 -- +-----------------+
 
--- Solution
+--SQL Statements
+create table logs(
+Id  integer,
+Num  integer
+);
+
+insert into logs values(1,1);
+insert into logs values(2,1);
+insert into logs values(3,1);
+insert into logs values(4,2);
+insert into logs values(5,1);
+insert into logs values(6,2);
+insert into logs values(7,2);
+
+--Solution : 
+select distinct num from (
+select num,lead(num) over(order by ID) as Next_Num,
+lag(num) over(order by ID) as Prev_Num
+from logs)
+where num=Next_Num and num=Prev_Num
+;
